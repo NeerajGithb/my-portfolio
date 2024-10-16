804f4db9a0f8580c5457c024679cb75d59bbbb1d
NeerajGitCode <neerajsh071@gmail.com>
2024-10-16 17:33:39 +0530

    no joom

--- app/components/Navbar2.js
+++ app/components/Navbar2.js
@@ -2,8 +2,8 @@ import React from "react";
 import Link from "next/link";
 const Navbar2 = () => {
   return (
-    <div className="max-sm:min-h-[60px] max-sm:max-h-[60px] xl:hidden px-2 lg:px-10 py-2  border-b-2 border-black shadow-lg bg-gradient-to-br from-gray-100 to-gray-300 fixed top-[64px] max-lg:top-[58px] max-sm:top-[60px] w-full z-[10000]">
-      <div className="flex justify-between sm:space-x-8 font-bold">
+    <div className="flex items-center max-sm:min-h-[50px] max-sm:max-h-[50px] xl:hidden px-2 lg:px-10 py-2  border-b-2 border-black shadow-lg bg-gradient-to-br from-gray-100 to-gray-300 fixed top-[64px] max-lg:top-[58px] max-sm:top-[60px] w-full z-[10000]">
+      <div className="flex justify-between w-full items-center sm:space-x-8 font-bold">
         <Link
           href="/about"
           className="hover:underline hover:scale-105 transition-transform duration-200">
--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import Navbar from "./components/Navbar";
 import Navbar2 from "./components/Navbar2";
 import Sidebar from "./components/Sidebar";
 import "./globals.css";
+import Head from "next/head"; // Import the Head component
 
 export const metadata = {
   title: "Neeraj Vishwakarma | Full-Stack Developer Portfolio",
@@ -15,10 +16,19 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+        />
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        <link rel="icon" href={metadata.icons.icon} />
+      </Head>
       <body>
         <Navbar />
         <Navbar2 />
-        <main className="">{children}</main>
+        <main>{children}</main>
         <Sidebar />
       </body>
     </html>
