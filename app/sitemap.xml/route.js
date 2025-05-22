ade3b1c1d455c64a307d60b5949a8f31be7ea5c3
Neeraj Vishwakarma <neerajvishwakarma726689@gmail.com>
2025-05-22 13:13:54 +0530

    route

--- app/sitemap.xml/route.js
+++ app/sitemap.xml/route.js
@@ -1,28 +1,15 @@
-// app/sitemap.xml/route.js
-export async function GET() {
-  const baseUrl = "https://my-portfolio-n.vercel.app"; // ✅ your deployed URL
-
-  const routes = [
-    "", "about", "contact", "projects", "resume", "skills",
-    "facebook", "music-web", "netfkix", "portfolio", "todo", "twitter", "url-shortner"
-  ];
-
-  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    ${routes
-      .map(
-        (route) => `
-      <url>
-        <loc>${baseUrl}/${route}</loc>
-        <lastmod>${new Date().toISOString()}</lastmod>
-      </url>
-    `)
-      .join("")}
-  </urlset>`;
-
-  return new Response(sitemap, {
-    headers: {
-      "Content-Type": "application/xml",
-    },
-  });
-}
+const routes = [
+  "",
+  "about",
+  "projects",
+  "contact",
+  "resume",
+  "skills",
+  "facebook",
+  "music-web",
+  "netfkix",
+  "portfolio",
+  "todo",
+  "twitter",
+  "url-shortner",
+];
